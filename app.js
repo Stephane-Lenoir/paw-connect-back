4f4f24390b14306a7fa91f306c80994fdeea9ce4
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-02 14:09:09 +0200

    update files and create a folder routers for routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
-import { router } from "./app/router.js";
+import { router } from "./app/routers/router.js";
 
 const app = express();
 
---
+++ app/routers/router.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { controllerWrapper as cw } from "../utils/controllerWrapper.js";
+
+export const router = Router();
+
+// Add all sub-routers here !
