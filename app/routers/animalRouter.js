f0ad95e8d345d3b8de5de08c7c4a57ebe21f68d1
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-02 15:20:31 +0200

    feat: animalController (getAllAnimals and getOneAnimal by ID) animalRouter

--- app/routers/animalRouter.js
+++ app/routers/animalRouter.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
 import { controllerWrapper as cw } from "../utils/controllerWrapper.js";
+import * as animalController from "../controllers/animalController.js";
 
 export const router = Router();
+
+router.get("/animals", cw(animalController.getAllAnimals));
+router.get("/animals/:id", cw(animalController.getOneAnimal));
