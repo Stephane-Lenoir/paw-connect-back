f79bfe1687b49047249748d750a683a36de805c6
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-03 12:16:44 +0200

    add gender

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -3,7 +3,7 @@ import { Animal } from "../models/Animal.js";
 export const searchFilter = async (req, res) => {
   // Get the search param from the body
 
-  const { species, location } = req.body;
+  const { species, location, gender } = req.body;
 
   const searchFilters = {};
 
@@ -13,6 +13,10 @@ export const searchFilter = async (req, res) => {
 
   if (location) {
     searchFilters.location = location;
+  }
+
+  if (gender) {
+    searchFilters.gender = gender;
   }
 
   const filters = await Animal.findAll({ where: searchFilters });
