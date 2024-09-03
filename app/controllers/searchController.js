f9730a704434d8810ef4204335792368f41dd144
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-03 13:45:08 +0200

    feat: add race and availability in filter

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -3,13 +3,15 @@ import { Animal } from "../models/Animal.js";
 export const searchFilter = async (req, res) => {
   // Get the search param from the body
 
-  const { species, location, gender } = req.body;
+  const { species, location, gender, availability, race } = req.body;
 
   const searchFilters = {};
 
   if (species) searchFilters.species = species;
   if (location) searchFilters.location = location;
   if (gender) searchFilters.gender = gender;
+  if (availability) searchFilters.availability = availability;
+  if (race) searchFilters.race = race;
 
   const filters = await Animal.findAll({ where: searchFilters });
   res.json(filters);
