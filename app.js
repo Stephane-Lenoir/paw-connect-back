1c283fafbac0c35e3ce241becb5928fad0eebd7f
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-05 11:27:15 +0200

    fix the port

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 await app.listen(port);
 console.log(`🚀 Server listening at http://localhost:${port}`);
