c0f796fbd3ae82d87e06b5eeebbf982349b367f7
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-10 21:36:05 +0200

    edit: password schema update

--- app/utils/validationSchemas.js
+++ app/utils/validationSchemas.js
@@ -25,13 +25,14 @@ export const searchSchema = Joi.object({
   age: Joi.number().integer().min(0).max(30),
 });
 
+// Validation schema for update
 export const updateSchema = Joi.object({
   name: Joi.string().optional().min(2).max(100),
-  password: Joi.string().optional(),
+  password: Joi.string().optional().min(12).max(255),
   firstname: Joi.string().optional().min(2).max(100),
 });
 
-// Schema for creation animal
+// Validation schema for creation animal
 export const createAnimalSchema = Joi.object({
   name: Joi.string().required().min(2).max(100),
   species: Joi.string().required().min(2).max(100),
@@ -44,6 +45,7 @@ export const createAnimalSchema = Joi.object({
   user_id: Joi.number().integer(),
 });
 
+// Validation schema for photo animal
 export const photoAnimalSchema = Joi.object({
   mimetype: Joi.string()
     .valid("image/jpeg", "image/png", "image/jpg", "image/gif")
@@ -51,6 +53,7 @@ export const photoAnimalSchema = Joi.object({
   size: Joi.number().max(1024 * 1024 * 1), // 1 pour 1024 mo
 });
 
+// Validation schema for update animal
 export const updateAnimalSchema = Joi.object({
   name: Joi.string().required().min(2).max(100),
   species: Joi.string().required().min(2).max(100),
