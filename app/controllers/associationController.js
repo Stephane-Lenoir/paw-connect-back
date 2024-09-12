e360ada979c034aba48ed4e81f6d7af3dc586f56
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-09-12 10:42:30 +0200

    edit: getOneAssociation

--- app/controllers/associationController.js
+++ app/controllers/associationController.js
@@ -2,19 +2,15 @@ import { User, Role, Request, Animal } from "../models/associations.js";
 
 // Method to retrieve association data
 export const getAllAssociations = async (req, res) => {
-  try {
-    const associations = await User.findAll({
-      attributes: { exclude: ["password"] },
-      include: {
-        model: Role,
-        as: "role",
-        where: { id: 3 },
-      },
-    });
-    res.status(200).json(associations);
-  } catch (error) {
-    res.status(500).json({ error: "An error has occurred." });
-  }
+  const associations = await User.findAll({
+    attributes: { exclude: ["password"] },
+    include: {
+      model: Role,
+      as: "role",
+      where: { id: 3 },
+    },
+  });
+  res.status(200).json(associations);
 };
 
 // Method to retrieve data for a single association
@@ -26,34 +22,34 @@ export const getOneAssociation = async (req, res) => {
       error: "Association not found. Please verify the provided ID.",
     });
   }
-  try {
-    const association = await User.findOne({
-      attributes: { exclude: ["password"] },
-      include: {
+  const association = await User.findOne({
+    attributes: { exclude: ["password"] },
+    where: { id: associationId },
+    include: [
+      {
         model: Role,
         as: "role",
         where: { id: 3 },
-        model: Request,
-        as: "requests",
-        where: { status: "En attente" },
-        include: [
-          {
-            model: Animal,
-            as: "request",
-          },
-        ],
       },
-      where: { id: associationId },
+      // {
+      //   model: Request,
+      //   as: "requests",
+      //   where: { status: "En attente" },
+      //   include: [
+      //     {
+      //       model: Animal,
+      //       as: "request",
+      //     },
+      //   ],
+      // },
+    ],
+  });
+
+  if (!association) {
+    return res.status(404).json({
+      error: "Association not found. Please verify the provided ID.",
     });
-
-    if (!association) {
-      return res.status(404).json({
-        error: "Association not found. Please verify the provided ID.",
-      });
-    }
-
-    res.status(200).json(association);
-  } catch (error) {
-    res.status(500).json({ error: "An error has occurred." });
   }
+
+  res.status(200).json(association);
 };
